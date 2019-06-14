aac100545fa0f9f0b89d238e13ae885b9007477d
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint
    I resolved conflicts between base64 and useragent branches.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
 
   res.end()
 })
@@ -13,9 +14,15 @@ function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
 
+
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
+}
+
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
 }
 
 server.listen(PORT)
--- test.js
+++ test.js
@@ -15,11 +15,22 @@ tape('should respond hello', (t) => {
   })
 })
 
+
 tape('should respond b64', (t) => {
   jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
     if (err) t.error(err)
 
     t.equal(body.b64, 'aGVsbG8=')
+    t.end()
+  })
+})
+
+tape('should respond user-agent', (t) => {
+  const opts = { headers: { 'User-Agent': 'tape' } }
+  jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.ua, 'tape')
     t.end()
   })
 })
